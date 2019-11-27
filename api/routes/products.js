d9e56d74c694ef660fd43b1c5d20c3e190a1ef3f
Mohd Shehzad Malik Momin <momin_m@India.XoriantCorp.com>
2019-11-27 18:35:30 +0530

    get image changes

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,15 +12,28 @@ const storage = multer.diskStorage({
     }
 });
 
+const fileFilter = (req, file, callback) => {
+    // reject a file
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        callback(null, true);
+    } else {
+        callback(null, false);
+    }
+}
+
 const upload = multer({
-    storage: storage
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
 });
 
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
         const response = {
@@ -29,6 +42,7 @@ router.get('/', (req, res, next) => {
                 return {
                     name: doc.name,
                     price: doc.price,
+                    productImage: doc.productImage,
                     _id: doc._id,
                     request: {
                         type: 'GET',
@@ -52,7 +66,8 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     });
 
     product.save().then(
@@ -80,7 +95,7 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(doc => {
         console.log(doc);
